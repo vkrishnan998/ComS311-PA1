da5afa26c827987c1d19aaa8016741b20e4fba22
Vignesh Krishnan <vigneshk@iastate.edu>
2019-03-27 21:55:52 -0500

    RBTree constructor and blank methods

--- src/RBTree.java
+++ src/RBTree.java
@@ -1,5 +1,28 @@
 
 
 public class RBTree {
-
+	Node root;
+	Node nil;
+	
+	public RBTree() {
+		
+	}
+	
+	public Node getRoot() {
+		return null;
+	}
+	
+	public Node getNILNode() {
+		return null;
+	}
+	
+	public int getSize() {
+		return 0;
+	}
+	
+	public int getHeight() {
+		return 0;
+	}
+	
+	
 }
