8375d8cc79102f4f211560f67d155d80bdf8bedb
Vignesh Krishnan <vigneshk@iastate.edu>
2019-03-27 23:10:59 -0500

    RBT size and height implementations

--- src/RBTree.java
+++ src/RBTree.java
@@ -3,26 +3,41 @@
 public class RBTree {
 	Node root;
 	Node nil;
+	int height;
+	int size;
 	
 	public RBTree() {
-		
+		root.color = 1;
+		nil.color = 1;
+		height = 1;
+		findBlackHeightAndSize(root);
 	}
 	
 	public Node getRoot() {
-		return null;
+		return root;
 	}
 	
 	public Node getNILNode() {
-		return null;
+		return nil;
 	}
 	
 	public int getSize() {
-		return 0;
+		return size;
 	}
 	
 	public int getHeight() {
-		return 0;
+		return height;
 	}
 	
-	
+	public void findBlackHeightAndSize(Node root) {
+		if (root == null) {
+			return;
+		}
+		if (root.color == 1) {
+			height++;
+		}
+		size++;
+		findBlackHeightAndSize(root);
+		findBlackHeightAndSize(root);
+	}
 }
