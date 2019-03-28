a007c4c7cc9b177ef7a57e7d5ee2ec16954704d4
Vatsal Bhatt <vbhatt22@iastate.edu>
2019-03-27 20:58:41 -0500

    Node class initial implementation

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,47 @@
 
-
 public class Node {
-
+	Node left;
+	Node right;
+	Node parent;
+	int color;
+	
+	Node getParent(){
+		return parent;
+	}
+	
+	Node getLeft(){
+		return left;
+	}
+	
+	Node getRight(){
+		return right;
+	}
+	
+	int getKey(){
+		return 0;
+	}
+	
+	int getP(){
+		return 0;
+	}
+	
+	int getVal(){
+		return 0;
+	}
+	
+	int getMaxVal(){
+		return 0;
+	}
+	
+	Endpoints getEndpoint(){
+		return null; 
+	}
+	
+	Endpoints getEmax(){
+		return null;
+	}
+	
+	int getColor(){
+		return color;
+	}
 }
