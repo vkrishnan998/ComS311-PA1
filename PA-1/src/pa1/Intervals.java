64a434414add7c1d469e13441f68bf4b5a2c629c
Vignesh Krishnan <vigneshk@iastate.edu>
2019-03-27 19:16:37 -0500

    Created classes and package

---
+++ PA-1/src/pa1/Intervals.java
@@ -0,0 +1,10 @@
+package pa1;
+
+public class Intervals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PA-1/src/pa1/Node.java
@@ -0,0 +1,10 @@
+package pa1;
+
+public class Node {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
